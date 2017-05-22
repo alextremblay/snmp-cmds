2b68aa0d80eb9e30127ce4380853249dff7f5b71
Alex Tremblay <alex.claude.tremblay@gmail.com>
2017-05-22 00:12:44 -0400

    Fixed an issue in the package's init process for ReadTheDocs

--- snmp_cmds/__init__.py
+++ snmp_cmds/__init__.py
@@ -5,11 +5,14 @@ from .api import Session
 
 # Run a test on import to ensure the net-snmp binaries are installed.
 from subprocess import run, DEVNULL
-try:
-    run('snmpget', stdout=DEVNULL, stderr=DEVNULL)
-except (FileNotFoundError, OSError):
-    raise ImportError(
-        'Net-SNMP does not appear to be installed on this system, '
-        'or the Net-SNMP commands are not on your PATH'
-    )
+from os import getenv
+if not getenv("READTHEDOCS"): # DON'T run this check if we're building
+    # documentation on ReadTheDocs
+    try:
+        run('snmpget', stdout=DEVNULL, stderr=DEVNULL)
+    except (FileNotFoundError, OSError):
+        raise ImportError(
+            'Net-SNMP does not appear to be installed on this system, '
+            'or the Net-SNMP commands are not on your PATH'
+        )
 del run, DEVNULL
